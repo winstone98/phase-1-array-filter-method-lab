14012514d1338e2a8e5439aa46b5726dda85b237
root <root@HomeLPT-001.localdomain>
2022-06-14 10:59:47 +0300

    Done with filters

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(element => element.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => {
+        return element[0].toUpperCase() === string[0].toUpperCase()
+    })
+}
+
+function matchName(array, string) {
+    return array.filter(element => {
+        return element.name === string
+    })
+}
